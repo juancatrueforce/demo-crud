e05406a61f90bc537cdbbf633a8c8b34b1aa0031
Juan Carlos Condori Machicado <juan.condori@trueforce.com>
2022-09-27 14:37:25 -0400

    :sparkles: add connection with postgres and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { UsersController } from './controllers/users.controller';
 import { ResourcesService } from './services/resources.service';
 import { UsersService } from './services/users.service';
 import { ResourcesController } from './controllers/resources.controller';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 
+const envFilePath: string = `${__dirname}/common/envs/.env`;
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+  ],
   controllers: [AppController, UsersController, ResourcesController],
   providers: [AppService, ResourcesService, UsersService],
 })
