cfd8e57efe6116be599aeb4a2eb7950f4aee6a43
Juan Carlos Condori Machicado <juan.condori@trueforce.com>
2022-09-28 13:02:23 -0400

    :sparkles: Encrypt password of users

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './../entities/user.entity';
 import { UserDto } from 'src/dtos/user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -23,15 +24,19 @@ export class UsersService {
     user.email = payload.email;
     user.firstname = payload.firstname;
     user.lastname = payload.lastname;
-    user.password = payload.password;
+    user.password = await this.encryptPassword(payload.password);
 
     return await this.userRepository.save(user);
   }
 
   async update(id: string, payload: UserDto) {
     let user = await this.findOne(id);
+    const newPayload = { ...payload };
     if (user) {
-      const responseUpdated = await this.userRepository.update(id, payload);
+      if (payload.password) {
+        newPayload.password = await this.encryptPassword(payload.password);
+      }
+      const responseUpdated = await this.userRepository.update(id, newPayload);
       return await this.findOne(id);
     }
     return null;
@@ -44,5 +49,10 @@ export class UsersService {
       return user;
     }
     return null;
+  }
+
+  private async encryptPassword(password: string) {
+    const saltOrRounds = 10;
+    return await bcrypt.hash(password, saltOrRounds);
   }
 }
